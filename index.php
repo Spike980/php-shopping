8967295cdc78a29ac324e5c5f74e9ad8fb50cce5
Anand Mallick <anandmallick980@gmail.com>
2016-08-20 13:36:24 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!Doctype html>
+<html lang = "en">
+<head>
+<title> My Shop </title>
+</head>
+<body background="images/back1.jpg">
+<div style="margin-left:250px;margin-top:0px">
+<img src="images/my_shop.jpg" align="center">
+</div>
+<h1 style="color:red" align="center"> Hello, User What's Your name: </h1>
+<fieldset style="width:25px;border-style:inset;margin-left:550px" >
+<form action="products.php" method=get>
+<input type="text" name="name" id="name" style="background:#808080;color:white">
+<input type="submit" name="enter" id="enter" style="border-radius:5px;width=25px;height:25px;background:blue;color:white;margin-left:45px">
+</form>
+</fieldset>
+</body>
+
